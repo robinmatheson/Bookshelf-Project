56ee21504ab7ff6e884641e2964fbf21e9f02340
Robin Matheson <robinm0211@gmail.com>
2023-07-26 13:23:09 -0700

    updated documentation in Event.java and EventLog.java

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -3,38 +3,28 @@ package model;
 import java.util.Calendar;
 import java.util.Date;
 
-// source for class: CPSC210 Alarm System project
+// source: Event class from Alarm project, CPSC 210
 
-/**
- * Represents an alarm system event.
- */
+// Represents a bookshelf event.
 public class Event {
+
     private static final int HASH_CONSTANT = 13;
     private final Date dateLogged;
     private final String description;
 
-    /**
-     * Creates an event with the given description
-     * and the current date/time stamp.
-     * @param description  a description of the event
-     */
+    // EFFECTS: Creates an event with the given description and the current date/time stamp
     public Event(String description) {
         dateLogged = Calendar.getInstance().getTime();
         this.description = description;
     }
 
-    /**
-     * Gets the date of this event (includes time).
-     * @return  the date of the event
-     */
+    // not used in project
+    // EFFECTS: returns date of event
     public Date getDate() {
         return dateLogged;
     }
 
-    /**
-     * Gets the description of this event.
-     * @return  the description of the event
-     */
+    // EFFECTS: returns the description of this event
     public String getDescription() {
         return description;
     }
@@ -58,9 +48,9 @@ public class Event {
         return (HASH_CONSTANT * dateLogged.hashCode() + description.hashCode());
     }
 
+    // EFFECTS: returns a readable string of this Event
     @Override
     public String toString() {
-        return dateLogged.toString() + "\n" + description;
+        return dateLogged.toString() + ": " + description;
     }
 }
-
